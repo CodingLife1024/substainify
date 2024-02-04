e9cfd7b3d757a941734d7c78ad3fd8842a0c79d6
Riddhidipta Pal <riddhidipta2003@gmail.com>
2024-02-04 15:51:52 +0530

    wefuweui

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -15,25 +15,64 @@ const Dashboard = () => {
         </div>
         <div className={styles.slide_container}>
           <div className={styles.bigslide}>
-            <div className={styles.slide} style={{ backgroundImage: 'url(/1.svg)' }}>1
+            <div className={styles.slide} style={{ backgroundImage: 'url(/1.svg)' }}>
+              <div className={styles.slide_title} style={{ marginBottom: "30px" }}>The Sustainability Dashboard: Your Window to Impact</div>
+              <div className={styles.slide_content_1}>
+                <p>The Sustainability Dashboard is a unique feature that sets Sustanify Hub apart. This tool empowers both businesses and consumers to take control of their sustainability journey, offering transparency, insights, and data-driven decision-making. Its key elements are:</p>
+              </div>
             </div>
-            <div className={styles.slide} style={{ backgroundImage: 'url(/2.svg)' }}>2
-
+            <div className={styles.slide} style={{ backgroundImage: 'url(/2.svg)' }}>
+              <div className={styles.slide_title} style={{ marginBottom: "20px" }}>For Businesses: </div>
+              <div className={styles.slide_content_2}>
+                <p style={{ marginBottom: "10px" }}>Tracking Sustainability Progress: Businesses can monitor and assess their sustainability initiatives, from reducing their carbon footprint to implementing waste reduction strategies. The dashboard allows them to measure and visualize their impact over time, providing essential insights for continuous improvement.</p>
+                <p style={{ marginBottom: "10px" }}>Carbon Footprint Reduction: We place a special emphasis on carbon footprint reduction. The dashboard displays real-time data on carbon emissions, allowing businesses to set targets and track their progress towards a lower carbon impact.</p>
+                <p style={{ marginBottom: "10px" }}>Waste Reduction: Waste management is a critical aspect of sustainability. The dashboard records waste reduction metrics, helping businesses identify opportunities to minimize waste generation and optimize resource use.</p>
+                <p style={{ marginBottom: "10px" }}>Eco-Friendly Materials Usage: Sustanify Hub encourages the use of eco-friendly materials. The dashboard keeps track of the types and quantities of such materials used, fostering accountability and promoting the adoption of sustainable alternatives.</p>
+              </div>
+              </div>
+            <div className={styles.slide} style={{ backgroundImage: 'url(/3.svg)' }}>
+              <div className={styles.slide_title} style={{ marginBottom: "20px" }}>For Consumers: </div>
+                <div className={styles.slide_content_2}>
+                  <p style={{ marginBottom: "10px" }}>Empowering Informed Choices: For consumers, the Sustainability Dashboard is a valuable tool for making informed choices. Each product's environmental impact is clearly displayed, allowing consumers to assess its contribution to carbon footprint reduction, waste reduction, and the use of eco-friendly materials.</p>
+                  <p style={{ marginBottom: "10px" }}>Personal Carbon Footprint Reduction: The dashboard goes a step further by enabling consumers to track their personal carbon footprint. By inputting data on their lifestyle and consumption habits, individuals can see the real-world impact of their choices and make adjustments as needed.</p>
+                  <p style={{ marginBottom: "10px" }}>Progress Over Time: Over time, consumers can see how their purchasing decisions have contributed to the reduction of carbon emissions and waste. This feature offers a sense of achievement and motivates users to continue supporting sustainable products.</p>
+                </div>
             </div>
-            <div className={styles.slide} style={{ backgroundImage: 'url(/3.svg)' }}>3
-
+            <div className={styles.slide} style={{ backgroundImage: 'url(/4.svg)' }}>
+              <div className={styles.slide_title} style={{ marginBottom: "30px" }}>Sustanify Hub's Annual Green Impact Report</div>
+              <div className={styles.slide_content_1}>
+                <p>At Sustanify Hub, we believe that transparency and accountability are fundamental to achieving our mission of promoting sustainable development and eco-friendly products. One of the ways we demonstrate this commitment is through our Annual Green Impact Report, a comprehensive document that not only showcases our achievements but also highlights the collective positive impact our platform and users have made on the environment and society.</p>
+              </div>
             </div>
-            <div className={styles.slide} style={{ backgroundImage: 'url(/4.svg)' }}>4
-
+            <div className={styles.slide} style={{ backgroundImage: 'url(/5.svg)' }}>
+              <div className={styles.slide_title} style={{ marginBottom: "30px" }}>The Product Rating Agency - Your Guide to Eco-Friendly Shopping</div>
+              <div className={styles.slide_content_1}>
+                <p>In a world where sustainable choices are paramount, it can be challenging for consumers to distinguish between genuinely eco-friendly products and those that merely claim to be. To address this issue, Sustanify Hub has introduced the Product Rating Agency, an impartial entity with a crucial role in evaluating and rating products available on our platform. We evaluate products based on a pass/fail system comprising of 10 factors.</p>
+              </div>
             </div>
-            <div className={styles.slide} style={{ backgroundImage: 'url(/5.svg)' }}>5
-
+            <div className={styles.slide} style={{ backgroundImage: 'url(/6.svg)' }}>
+              <div className={styles.slide_title} style={{ marginBottom: "20px" }}>Pass/Fail Evaluation System </div>
+                <div className={styles.slide_content_2}>
+                  <p style={{ marginBottom: "0px" }}>Feature 1: Carbon Footprint</p>
+                  <p style={{ marginBottom: "0px" }}>Feature 2: Water Usage</p>
+                  <p style={{ marginBottom: "0px" }}>Feature 3: Waste Reduction</p>
+                  <p style={{ marginBottom: "0px" }}>Feature 4: Sustainable Materials</p>
+                  <p style={{ marginBottom: "0px" }}>Feature 5: Supply Chain Transparency</p>
+                  <p style={{ marginBottom: "0px" }}>Feature 6: End-of-Life Considerations</p>
+                  <p style={{ marginBottom: "0px" }}>Feature 7: Fair Labor Practices</p>
+                  <p style={{ marginBottom: "0px" }}>Feature 8: Energy Efficiency</p>
+                  <p style={{ marginBottom: "0px" }}>Feature 9: Biodiversity Preservation</p>
+                  <p style={{ marginBottom: "0px" }}>Feature 10: Contribution to Sustainable Development Goals (SDGs)</p>
+                </div>
             </div>
-            <div className={styles.slide} style={{ backgroundImage: 'url(/6.svg)' }}>6
-
-            </div>
-            <div className={styles.slide} style={{ backgroundImage: 'url(/7.svg)' }}>7
-
+            <div className={styles.slide} style={{ backgroundImage: 'url(/7.svg)' }}>
+              <div className={styles.slide_title} style={{ marginBottom: "30px" }}>Blockchain-Enabled Carbon Credits: A New Era of Sustainability</div>
+              <div className={styles.slide_content_2}>
+                <p>In the pursuit of a greener, more sustainable world, carbon credits play a vital role. They provide a means for businesses and individuals to offset their carbon emissions by investing in projects that reduce or capture an equivalent amount of carbon elsewhere. However, the traditional carbon credit market has often faced challenges related to transparency, accountability, and accessibility.</p>
+              </div>
+              <div className={styles.slide_content_2}>
+                <p>Sustanify Hub is committed to revolutionizing the carbon credit market through blockchain technology. Blockchain offers a decentralized and immutable ledger that can ensure the transparency and reliability of carbon credit transactions, making it a powerful tool for achieving our sustainability goals.</p>
+              </div>
             </div>
           </div>
         </div>
--- src/pages/Vision/Vision.js
+++ src/pages/Vision/Vision.js
@@ -9,29 +9,70 @@ const Vision = () => {
       <Topbar />
       <div className={styles.main}>
         <div className={styles.title}>
-          Dashboard
+          Our Vision
         </div>
         <div className={styles.slide_container}>
           <div className={styles.bigslide}>
             <div className={styles.slide} style={{ backgroundImage: 'url(/1.svg)' }}>1
             </div>
-            <div className={styles.slide} style={{ backgroundImage: 'url(/2.svg)' }}>2
-
+            <div className={styles.slide} style={{ backgroundImage: 'url(/2.svg)' }}>
+              <div className={styles.slide_title} style={{ marginBottom: "20px" }}>Empowering Local Sustainability: Sustainify Hub's Vision for Underdeveloped Countries</div>
+              <div className={styles.slide_content_1}>
+                <p>Sustanify Hub's commitment to sustainability extends beyond merely offering eco-friendly products; it encompasses a comprehensive approach that seeks to revolutionize supply chains. In this section, we introduce an innovative idea – setting up subsidiaries in underdeveloped countries. We will also discuss the significant benefits of a franchise model for both sustainability and affordability.</p>
+              </div>
             </div>
-            <div className={styles.slide} style={{ backgroundImage: 'url(/3.svg)' }}>3
-
+            <div className={styles.slide} style={{ backgroundImage: 'url(/3.svg)' }}>
+              <div className={styles.slide_title} style={{ marginBottom: "10px" }}>Setting Up Subsidiaries in Underdeveloped Countries</div>
+              <div className={styles.slide_content_2}>
+                <p style={{ marginBottom: "10px" }}>Localization: We select the top 50 eco-friendly products annually and produce them in underdeveloped countries, taking into account the specific needs and demands of those regions. This approach aligns with our commitment to sustainability, as it reduces the carbon footprint associated with international shipping.</p>
+                <p style={{ marginBottom: "10px" }}>Partnerships: We collaborate with local representatives and establish subsidiaries as employee-owned corporations. This ensures that the local community is actively involved and benefits from our operations.</p>
+                <p style={{ marginBottom: "10px" }}>Localization: We select the top 50 eco-friendly products annually and produce them in underdeveloped countries, taking into account the specific needs and demands of those regions. This approach aligns with our commitment to sustainability, as it reduces the carbon footprint associated with international shipping.</p>
+                <p style={{ marginBottom: "10px" }}>Affordability: By producing these products locally, we can significantly reduce the cost, making them more affordable to a wider population, including those who could not previously access sustainable options.</p>
+                <p style={{ marginBottom: "10px" }}>Royalty and Consultation Services: Companies that originally produce these products receive royalties for sales in other countries, incentivizing their participation in our global sustainability efforts. Our corporation charges a nominal transaction cost (0.1%) for consultation services, which also contributes to sustainability initiatives.</p>
+              </div>
             </div>
-            <div className={styles.slide} style={{ backgroundImage: 'url(/4.svg)' }}>4
-
+            <div className={styles.slide} style={{ backgroundImage: 'url(/4.svg)' }}>
+              <div className={styles.slide_title} style={{ marginBottom: "10px" }}>Franchise Model for Sustainability and Affordability</div>
+              <div className={styles.slide_content_2}>
+                <p style={{ marginBottom: "10px" }}>Local Empowerment: Subsidiaries are often employee-owned, ensuring that the local population is involved in decision-making and benefits from the business operations.</p>
+                <p style={{ marginBottom: "10px" }}>Affordability: Producing eco-friendly products locally significantly reduces costs, making them more accessible to a broader demographic, including those in underdeveloped countries.</p>
+                <p style={{ marginBottom: "10px" }}>Reduced Carbon Footprint: Local production reduces the environmental impact associated with long-distance shipping, contributing to a reduction in greenhouse gas emissions.</p>
+                <p style={{ marginBottom: "10px" }}>Economic Development: Establishing subsidiaries creates job opportunities, fosters entrepreneurship, and stimulates economic development in underdeveloped regions.</p>
+                <p style={{ marginBottom: "10px" }}>By employing a franchise model, Sustanify Hub not only furthers its commitment to sustainability but also extends the benefits of eco-friendly products to those who may have been excluded due to financial constraints. This innovative approach aligns with our vision of a world where sustainability is accessible to all, regardless of their geographical location or economic status.</p>
+              </div>
             </div>
-            <div className={styles.slide} style={{ backgroundImage: 'url(/5.svg)' }}>5
-
+            <div className={styles.slide} style={{ backgroundImage: 'url(/5.svg)' }}>
+              <div className={styles.slide_title} style={{ marginBottom: "10px" }}>Cost-Saving Measures: A Sustainify Hub Signature</div>
+              <div className={styles.slide_content_2}>
+                <p style={{ marginBottom: "10px" }}>Masses Over Perfection: Sustainability should be for the masses, not the few. We recognize that the pursuit of perfection in sustainability can often lead to elevated costs. Our approach prioritizes access over perfection, focusing on cost-saving measures that ensure a wide range of eco-friendly products is available and accessible to everyone.</p>
+                <p style={{ marginBottom: "10px" }}>Engineering Sustainability: At Sustainify Hub, we have a dedicated team of engineers and MBAs who scrutinize products to evaluate their cost-saving potential. By identifying alternative, budget-friendly materials or streamlining production methods, we advice suppliers new way of production and we aim to offer a diverse range of products that are both eco-friendly and budget-friendly.</p>
+                <p style={{ marginBottom: "10px" }}>Inclusive Outreach: Our marketing and outreach strategies are designed to reach and resonate with people from all walks of life. We make sustainability relatable and achievable, demonstrating that even small changes in consumer behavior can have a significant impact on the environment and society.</p>
+              </div>
             </div>
-            <div className={styles.slide} style={{ backgroundImage: 'url(/6.svg)' }}>6
-
+            <div className={styles.slide} style={{ backgroundImage: 'url(/6.svg)' }}>
+              <div className={styles.slide_title} style={{ marginBottom: "10px" }}>Community Engagement Campaign: "Sustainify Together"</div>
+              <div className={styles.slide_content_2}>
+                <p style={{ marginBottom: "10px" }}>The "Sustainify Together" campaign is designed to engage and sensitize our intended target segments to the The Sustainify Hub project. By fostering a sense of community and shared responsibility, we aim to inspire individuals and businesses to embrace sustainability, affordability, and accessibility. This campaign leverages digital and grassroots efforts to create a powerful movement for positive change.</p>
+                <p style={{ marginBottom: "10px" }}>Partnerships with Schools and Universities:</p>
+                <ul className={styles.ull}>
+                  <li>Implementing educational programs in schools and universities to instill sustainable values in the younger generation.</li>
+                  <li> Encouraging students to explore sustainable projects and engage with The Sustainify Hub's initiatives.</li>
+                </ul>
+                <p style={{ marginBottom: "10px" }}>Business Partnerships:</p>
+                <ul className={styles.ull}>
+                  <li>Collaborating with local businesses to offer discounts or special promotions on eco-friendly products  available on The Sustanify Hub.</li>
+                  <li>We will highlight these partnerships as examples of the impact of choosing sustainable options.</li>
+                </ul>
+              </div>
             </div>
-            <div className={styles.slide} style={{ backgroundImage: 'url(/7.svg)' }}>7
-
+            <div className={styles.slide} style={{ backgroundImage: 'url(/7.svg)' }}>
+              <div className={styles.slide_title} style={{ marginBottom: "10px" }}>Blockchain-Enabled Carbon Credits: Benefits</div>
+              <div className={styles.slide_content_2}>
+                <p style={{ marginBottom: "10px" }}>Transparency and Trust: Blockchain records every carbon credit transaction in a transparent and unchangeable ledger, providing a clear and indisputable record of emissions reductions. This transparency fosters trust among all parties involved, from businesses and individuals to project developers and verifiers.</p>
+                <p style={{ marginBottom: "10px" }}>Accessibility: By utilizing blockchain, we can lower the barriers to entry in the carbon credit market. This means that even small businesses, individuals, and organizations with limited resources can participate in carbon offsetting initiatives, democratizing the fight against climate change.</p>
+                <p style={{ marginBottom: "10px" }}>Reduced Transaction Costs: Blockchain technology eliminates intermediaries, reducing transaction costs associated with carbon credit trading. This cost-effectiveness makes it more attractive for businesses and individuals to invest in sustainability.</p>
+                <p style={{ marginBottom: "10px" }}>Innovation and Collaboration: Blockchain-enabled carbon credits open the door to new levels of innovation and collaboration. Smart contracts can automate the process, ensuring that credits are only released when the agreed-upon emissions reductions are achieved. This guarantees the integrity of the credits and reduces the need for manual verification.</p>
+              </div>
             </div>
           </div>
         </div>
