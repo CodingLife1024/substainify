1fae55bd3aaa4e513b9f17134c2679c670a45903
Riddhidipta Pal <riddhidipta2003@gmail.com>
2024-02-07 00:10:18 +0530

    hh

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.mjs
+const nextConfig = {
+    // Add the following line to enable static HTML export
+    exportPathMap: function () {
+      return {
+        '/': { page: '/' },
+        // Add more routes as needed
+      };
+    },
+  };
 
-export default nextConfig;
+export { nextConfig };
