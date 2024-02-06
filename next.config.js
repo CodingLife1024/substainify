3d0890ac055b183d43275007254fdfba493240dc
Riddhidipta Pal <riddhidipta2003@gmail.com>
2024-02-07 03:26:19 +0530

    h

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 // next.config.js
 const nextConfig = {
+  output: 'export',
   // Add the following function to generate static export parameters
   async generateStaticParams() {
     // Define your static paths here
