cccf07b497fbfdd4911c90904128bfa19e4205d8
Riddhidipta Pal <riddhidipta2003@gmail.com>
2024-02-07 03:34:33 +0530

    jj

--- next.config.js
+++ next.config.js
@@ -21,14 +21,8 @@
 const nextConfig = {
   output: 'export',
 
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
-
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
-
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
+  // Optional configurations can be added here
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
