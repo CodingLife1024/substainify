b03090555500ad4d171d0c8527276347c090e8bf
Riddhidipta Pal <riddhidipta2003@gmail.com>
2024-02-07 03:30:52 +0530

    ff

--- next.config.js
+++ next.config.js
@@ -1,16 +1,34 @@
 // next.config.js
+// const nextConfig = {
+//   output: 'export',
+//   // Add the following function to generate static export parameters
+//   async generateStaticParams() {
+//     // Define your static paths here
+//     const paths = {
+//       '/': { page: '/' },
+//       // Add more routes as needed
+//     };
+
+//     return paths;
+//   },
+// };
+
+// module.exports = nextConfig;
+
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
   output: 'export',
-  // Add the following function to generate static export parameters
-  // async generateStaticParams() {
-  //   // Define your static paths here
-  //   const paths = {
-  //     '/': { page: '/' },
-  //     // Add more routes as needed
-  //   };
 
-  //   return paths;
-  // },
-};
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
 
-module.exports = nextConfig;
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+
+module.exports = nextConfig
