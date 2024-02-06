9e080dcaa6ae72de12d288a0c3df11d1d57c65a4
Riddhidipta Pal <riddhidipta2003@gmail.com>
2024-02-07 03:20:41 +0530

    uwehw

---
+++ next.config.js
@@ -0,0 +1,15 @@
+// next.config.js
+const nextConfig = {
+  // Add the following function to generate static export parameters
+  async generateStaticParams() {
+    // Define your static paths here
+    const paths = {
+      '/': { page: '/' },
+      // Add more routes as needed
+    };
+
+    return paths;
+  },
+};
+
+module.exports = nextConfig;
